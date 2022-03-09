d0274848f36dbd48d7218dd73a8de95dd8978aac
SSlivic <sanja.slivic@gmail.com>
2022-03-09 03:49:32 -0800

    Add files via upload
    files

---
+++ valentina-kolander.ga/trgovina_valentinakolander/index.php
@@ -0,0 +1,38 @@
+<html>
+	<head>
+		<title>Trgovina</title>
+		<meta charset="utf-8">
+		<meta name="author" value="Sanja Šlivić">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		
+	</head>
+	<body onload>
+  <nav>
+      <div class="hamburger">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+      <ul class="nav-links">
+      <li><a href="#">Začetna</a></li>
+        <li><a href="stranice/pocetna.html">O avtorju</a></li>
+        <li><a href="stranice/prikaz_revij.html">Prikaz revij</a></li>
+        <li><a href="../index.php">Trgovina</a></li>
+      </ul>
+    </nav>
+    <div class="pozadina">
+      <div class="prazan_prostor"></div>
+<div class="type" data-splitting>
+<h1>Valentina Kolander</h1>
+<h3>modna oblikovalka</h3>
+</div>
+    </div>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+ <script src="js/splitting.js"></script>
+ <script> Splitting(); </script>
+ <script src="js/app.js"></script>
+
+</body>
+</html>
