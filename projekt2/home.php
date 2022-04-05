80779dc215bb62fb1ae113e0551919fb18396076
SSlivic <sanja.slivic@gmail.com>
2022-04-05 13:59:10 +0200

    Add files via upload

---
+++ projekt2/home.php
@@ -0,0 +1,13 @@
+<?php
+include("index.php");
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  <?php  echo "Pozdravljeni ".$_POST ["username"]; ?>
+  </body>
+</html>
